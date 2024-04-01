25ae60eddb4fa290e0f18e2a70e81e79f0a85e6e
Cimpean Alexandru <115628859+cimpeanalexandru@users.noreply.github.com>
2024-04-01 17:38:06 +0300

    better fit on phones

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ var typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true
 })
+
+/* =============== Aside =============== */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.lenght;
+    for(let i=0; i<totalNavList; i++) {
+      const a = navList[i].querySelector("a");
+      a.addEventListener("click", function() {
+        console.log(this)
+      })
+    }
